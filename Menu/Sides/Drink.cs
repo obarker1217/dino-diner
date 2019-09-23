0f69095e18f991d8ecc7bb2e03e8f93e1573c0b8
Owen Barker <obarker1217@ksu.edu>
2019-09-23 17:02:13 -0500

    Worked on M3

---
+++ Menu/Sides/Drink.cs
@@ -0,0 +1,76 @@
+﻿/* Drink.cs
+ * Author: Owen Barker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum SodasaurusFlavor
+    {
+        Cola,
+        Orange,
+        Lime,
+        Cherry,
+        Vanilla,
+        RootBeer,
+        //Grape,
+        Chocolate//,
+        //Cream
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Drink
+    {
+        /// <summary>
+        /// This is the read-only list of ingredients.
+        /// </summary>
+        protected List<string> ingredients = new List<string>();
+
+        /// <summary>
+        /// Gets and sets the price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Gets and sets the calories
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Ice { get; set; }
+
+        /// <summary>
+        /// Gets the ingredients list
+        /// </summary>
+        public List<string> Ingredients
+        {
+            get
+            {
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the size
+        /// </summary>
+        public virtual Size Size { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void HoldIce()
+        {
+            this.Ice = false;
+        }
+    }
+}
---
+++ Menu/Sides/Sodasaurus.cs
@@ -0,0 +1,68 @@
+﻿/* Sodasaurus.cs
+ * Author: Owen Barker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Sodasaurus : Drink
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public SodasaurusFlavor Flavor { get; set; }
+
+        /// <summary>
+        /// This private property allows the switch case to function based on a change of order size.
+        /// </summary>
+        private Size size;
+
+        /// <summary>
+        /// This overridden method allows the calories and price of the menu item to change based on order size.
+        /// </summary>
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 1.50;
+                        Calories = 112;
+                        break;
+
+                    case Size.Medium:
+                        Price = 2.00;
+                        Calories = 156;
+                        break;
+
+                    case Size.Large:
+                        Price = 2.50;
+                        Calories = 208;
+                        break;
+                }
+            }
+            get
+            {
+                return size;
+            }
+        }
+
+
+        public Sodasaurus()
+        {
+            Price = 1.50;
+            Calories = 112;
+            Ingredients.Add("Water");
+            Ingredients.Add("Natural Flavors");
+            Ingredients.Add("Cane Sugar");
+        }
+    }
+}
