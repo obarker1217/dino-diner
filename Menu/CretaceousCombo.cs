8781d2c98ed77ca622dc9473f6b0c4e12dfe3201
Owen Barker <obarker1217@ksu.edu>
2019-09-27 16:21:20 -0500

    Did the CretaceousCombo

---
+++ Menu/CretaceousCombo.cs
@@ -0,0 +1,132 @@
+﻿/* CretaceousCombo.cs
+ * Author: Owen Barker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Entrees;
+using DinoDiner.Menu.Sides;
+using DinoDiner.Menu.Drinks;
+
+namespace DinoDiner.Menu
+{
+    public class CretaceousCombo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Entree Entree { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Side side;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Side Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                side = value;
+                side.Size = size;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Drink drink;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Drink Drink
+        {
+            get
+            {
+                return drink;
+            }
+            set
+            {
+                drink = value;
+                drink.Size = size;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return Entree.Price + Drink.Price + Side.Price - 0.25;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return Entree.Calories + Drink.Calories + Side.Calories;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Size size = Size.Small;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+                Drink.Size = value;
+                Side.Size = value;    
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                ingredients.AddRange(Entree.Ingredients);
+                ingredients.AddRange(Drink.Ingredients);
+                ingredients.AddRange(Side.Ingredients);
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CretaceousCombo(Entree entree)
+        {
+            Entree = entree;
+            Side = new Fryceritops();
+            Drink = new Sodasaurus();
+        }
+    }
+}
---
+++ MenuTest/CretaceousComboTest.cs
@@ -0,0 +1,19 @@
+﻿/* CretaceousCombo.cs
+ * Author: Owen Barker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Entrees;
+using DinoDiner.Menu.Sides;
+using DinoDiner.Menu.Drinks;
+using DinoDiner.Menu;
+
+namespace MenuTest
+{
+    public class CretaceousComboTest
+    {
+        CretaceousCombo combo = new CretaceousCombo();
+    }
+}
