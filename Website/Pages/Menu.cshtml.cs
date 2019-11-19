268f8d60c59a306dacf06ae285bff5ac4481d233
obarker1217 <obarker1217@ksu.edu>
2019-11-18 22:12:05 -0600

    Did some work on the milestone.

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+        public Menu Menu { get; }
+        /*
+
+        public IMenuItem MenuItem { get; }
+
+        public CretaceousCombo Combo { get; }
+
+        public Entree Entree { get; }
+
+        public Side Side { get; }
+
+        public Drink Drink { get; }
+        */
+
         public void OnGet()
         {
-
+            
         }
     }
 }
